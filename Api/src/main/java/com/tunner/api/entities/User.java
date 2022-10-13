1e574e06b818b5b29e796a697d62d0a2b779a486
pedro030 <pedrocontreras182@gmail.com>
2022-10-13 17:23:27 -0300

    create project

---
+++ Api/src/main/java/com/tunner/api/entities/User.java
@@ -0,0 +1,30 @@
+package com.tunner.api.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+    @Column(name = "lastname")
+    private String lastname;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "password")
+    private String password;
+
+}
